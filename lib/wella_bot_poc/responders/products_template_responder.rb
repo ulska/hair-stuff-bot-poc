c335a3351b2e071fbb7e9141e8c4142e12131d5f
Urszula Kowalska <ua.kowalska@gmail.com>
2017-06-29 08:13:45 +0200

    Add searching for products in users input

--- lib/wella_bot_poc/responders/products_template_responder.rb
+++ lib/wella_bot_poc/responders/products_template_responder.rb
@@ -13,7 +13,7 @@ class ProductsTemplateResponder
   end
 
   def message
-    get_matching_products.any? ? matching_products_message : nothing_found_message
+    products_from_input.any? ? matching_products_message : nothing_found_message
   end
 
   def nothing_found_message
@@ -22,12 +22,20 @@ class ProductsTemplateResponder
 
   def matching_products_message
     { text: I18n.t('messages.found_products') }
-    { attachment: GenericTemplate.new(get_matching_products).to_hash }
+    { attachment: GenericTemplate.new(products_from_input).to_hash }
   end
 
-  def get_matching_products
+  def input
+    @input ||= user_input.downcase
+  end
+
+  def products_from_input
+    input.split(' ').map { |in_part| get_matching_products(in_part) }.flatten
+  end
+
+  def get_matching_products(input)
     I18n.t('products').select do |product|
-      product['name'].downcase.include?(user_input.downcase)
+      product['name'].downcase.include?(input.downcase)
     end
   end
 end
